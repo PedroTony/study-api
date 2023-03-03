c7290a5147c06ddd8c9ffaeaee59d2b73951e174
Logon Aluno <logonrmlocal@fiap.com.br>
2023-03-02 22:15:17 -0300

    Criado PessoaController

---
+++ src/main/java/com/github/pedrotony/study_api/controller/PessoaController.java
@@ -0,0 +1,12 @@
+package com.github.pedrotony.study_api.controller;
+
+import java.util.List;
+import com.github.pedrotony.study_api.model.Pessoa;
+
+public class PessoaController {
+	
+	public List<Pessoa> listAll() {
+		return null;
+	}
+	
+}
