d500c5524ed5d5b93f6c991a657d79c395f371d4
Logon Aluno <logonrmlocal@fiap.com.br>
2023-03-30 22:25:34 -0300

    ListAll funcionando

--- src/main/java/com/github/pedrotony/study_api/controller/SearchedPessoa.java
+++ src/main/java/com/github/pedrotony/study_api/controller/SearchedPessoa.java
@@ -1,5 +1,7 @@
 package com.github.pedrotony.study_api.controller;
 
+import com.github.pedrotony.study_api.model.Pessoa;
+
 public class SearchedPessoa {
 	private long id;
 	private String nome;
@@ -15,5 +17,11 @@ public class SearchedPessoa {
 	}
 	public void setNome(String nome) {
 		this.nome = nome;
+	}
+	public static SearchedPessoa toDto(Pessoa pessoa) {
+		SearchedPessoa dto = new SearchedPessoa();
+		dto.setId(pessoa.getId());
+		dto.setNome(pessoa.getNome());
+		return dto;
 	}
 }
